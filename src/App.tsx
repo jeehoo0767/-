5bd483032f9aad94bf1b456855007c3dd4a2bc2d
jeehoo0767 <jeehoo0767@naver.com>
2025-01-23 03:01:39 +0900

    스타일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import DilutionCalculator from "./component/DilutionCalculator.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div>
-      <DilutionCalculator />
-    </div>
-  )
+  return <DilutionCalculator />
 }
 
 export default App
